a10959044607e7807c02bc373172eb52ce216c0e
장성준 <g6y116@gmail.com>
2023-11-23 18:13:42 +0900

    mysql로 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 insert into site_user (username, password, email) values ('qwer', 'qwerqwer', 'g6y116@naver.com');
-insert into question (subject, content) values ('sbb가 무엇인가요?', 'sbb에 대해서 알고 싶습니다.');
-insert into question (subject, content) values ('스프링부트 모델 질문입니다.', 'id는 자동으로 생성되나요?');
-insert into answer (content) values ('네 자동으로 생성됩니다.');
+--insert into question (subject, content) values ('sbb가 무엇인가요?', 'sbb에 대해서 알고 싶습니다.');
+--insert into question (subject, content) values ('스프링부트 모델 질문입니다.', 'id는 자동으로 생성되나요?');
+--insert into answer (content) values ('네 자동으로 생성됩니다.');
